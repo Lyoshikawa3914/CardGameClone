208dc74e64d88508e74d8046f9789c0b4512e2a9
Lawrence Yoshikawa <lyoshikawa3914@gmail.com>
2023-12-20 09:26:45 -0800

    updated HomePage

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,8 +1,10 @@
 function HomePage() {
     return (
-        <div>
-            <div className="navbar">
-                
+        <div className="flex flex-col p-0 m-0">
+            <div className="navbar flex justify-start w-full h-14 bg-sky-500">
+                <div className="flex items-center justify-center">
+                    <h1>Digimon Card Game Sim</h1>
+                </div>
             </div>
 
             <div className="searchBar">
