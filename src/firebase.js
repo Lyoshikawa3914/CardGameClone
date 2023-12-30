fed84c326baff2ff113270773fe99df8f127457c
Lawrence Yoshikawa <lyoshikawa3914@gmail.com>
2023-12-29 16:11:14 -0800

    added firebase and google button to landing page

--- src/firebase.js
+++ src/firebase.js
@@ -8,13 +8,7 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBtNeRfqvw5s1t2oBgf2c5THaJ8qXg4zls",
-  authDomain: "chat-app-fcb96.firebaseapp.com",
-  projectId: "chat-app-fcb96",
-  storageBucket: "chat-app-fcb96.appspot.com",
-  messagingSenderId: "957030913110",
-  appId: "1:957030913110:web:c71e961284414da639d7f7",
-  measurementId: "G-J0K9Q7S97C"
+  
 };
 
 // Initialize Firebase
